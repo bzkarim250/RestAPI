97a3bddd529e847b8406df9dc7787cc9cba3d880
bzkarim250 <bzkarim250@gmail.com>
2023-01-25 12:21:13 +0200

    added input validation

--- model/User.js
+++ model/User.js
@@ -1,23 +1,33 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
+import emailValidator from 'email-validator';
 
-const userSchema=new mongoose.Schema({
-    name:{
-        type:String,
-        required:true
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        minlength: 4,
+        maxlength: 30,
+        required: true
     },
-    email:{
-        type:String,
-        required:true,
-        unique:true
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        validate: {
+            validator: emailValidator.validate,
+            message: '{VALUE} is not a valid email address'
+        }
     },
-    password:{
-        type:String,
-        required:true
+    password: {
+        type: String,
+        required: true
     },
-    age:{
-        type:Number
+    age: {
+        type: Number,
+        min: 18,
+        max: 80,
+        required: true
     }
 },{timestamps:true});
 
-const User=mongoose.model('User',userSchema);
+const User = mongoose.model('User', userSchema);
 export default User;
