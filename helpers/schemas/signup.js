b006a9c7ba8d9d2596ec455517398a21a6508b0a
bzkarim250 <bzkarim250@gmail.com>
2023-01-26 13:20:38 +0200

    more signup validation

--- helpers/schemas/signup.js
+++ helpers/schemas/signup.js
@@ -2,16 +2,16 @@ import Joi from "joi";
 
 export const signupSchema = Joi.object({
     name:Joi.string()
-        .pattern(new RegExp('^[a-zA-Z]+$'))
+        .pattern(new RegExp('^[a-zA-Z]+(?:[- ]?[a-zA-Z]+)*$'))
         .required()
     ,
     email:Joi.string()
-        .pattern(new RegExp('^[A-Z0-9+_.-]+@[A-Z0-9.-]+$'))
+        .pattern(new RegExp('^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[A-Z|a-z]{2,}$'))
         .lowercase()
         .required()
     ,
     password:Joi.string()
-    .pattern(new RegExp('^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$'))
+    .pattern(new RegExp('^(?=.[a-z])(?=.[A-Z])(?=.[0-9])(?=.[!@#$%^&*])(?=.{8,})'))
     .required()
     ,
 });
--- middlewares/validation.js
+++ middlewares/validation.js
@@ -3,6 +3,15 @@ import { signupSchema } from "../helpers/schemas/signup";
 const validate=(req,res,next)=>{
     const { error } = signupSchema.validate(req.body);
     if(error){
+        if(error.message.includes('^[a-zA-Z]+(?:[- ]?[a-zA-Z]+)*$')){
+            res.status(400).json({message:"Name is invalid"});
+        }
+        if(error.message.includes('^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[A-Z|a-z]{2,}$')){
+            res.status(400).json({message:"Email is not valid"});
+        }
+        if(error.message.includes('^(?=.[a-z])(?=.[A-Z])(?=.[0-9])(?=.[!@#$%^&*])(?=.{8,})')){
+            res.status(400).json({message:"password must contain at least one uppercase letter, one lowercase letter, one digit, one special character and at least 8 characters"});
+        }
         res.status(400).json({message:error.details[0].message});
         return;
     }
