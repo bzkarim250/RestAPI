d7fea5504e4e23448d6379af4f066418ca04d9be
bzkarim250 <bzkarim250@gmail.com>
2023-01-29 11:59:51 +0200

    ft(validation) -implement signup validation [WIP ft-validation]

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import authController from "../controllers/Auth";
-import validate from '../middlewares/validation';
+import {validate} from '../middlewares/validation';
+import signupSchema from '../validation/signup';
 
 const route=express.Router();
-route.post('/signup',validate,authController.signup);
+route.post('/signup',validate(signupSchema),authController.signup);
 route.get('/user/:id',authController.singleUser);
 route.post('/login',authController.login);
 export default route;
